4572570d270252f2d3348637d0bf940b640291d4
JenVest2020 <jenvest2019@gmail.com>
2020-09-08 23:32:01 -0500

    index.js completed &logger middleWare completed

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code away!
+const server = require('./server.js');
+server.listen(5000, () => {
+    console.log('\n***server is running on http://localhost:5000***\n');
+});
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
+server.use(express.json());
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+server.use(logger);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +15,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.originalUrl}`, new Date());
+
+  next();
+}
 
 module.exports = server;
